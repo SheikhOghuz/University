cf97db61106724a847c8eac86581b9fc772f049a
Oghuz Ali <113483494+SheikhOghuz@users.noreply.github.com>
2022-11-29 19:28:56 -0800

    Update university.js

--- university.js
+++ university.js
@@ -2,11 +2,9 @@ $(document).ready(function() {
     $.get("http://universities.hipolabs.com/search?country=Canada", function(data, status) {
         if (status == "success" && data != null && data != undefined) {
             for (let index = 0; index < data.length; index++) {
-                $("#canadaUni").append(`<a target="_blank" href="${data[index].web_pages[0]}" class="uni-list-item uni-list-item-action">${data[index].name}</a>`);
+                $("#canadaUni").append(`<a target="_blank" href="${data[index].web_pages[0]}" class="uniList-item uniList-item-action">${data[index].name}</a>`);
 
             }
-        } else {
-            $("#canadaUni").append("List won't show");
         }
     });
 });
