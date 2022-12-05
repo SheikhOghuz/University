5608c5f1cafc147cec2f48014b050524dfe7b1ef
Oghuz Ali <113483494+SheikhOghuz@users.noreply.github.com>
2022-12-04 16:35:56 -0800

    Update
    Almas Ataman

--- university.js
+++ university.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
     $.get("http://universities.hipolabs.com/search?country=Canada", function(data, status) {
         if (status == "success" && data != null && data != undefined) {
             for (let index = 0; index < data.length; index++) {
-                $("#canadaUni").append(`<a target="_blank" href="${data[index].web_pages[0]}" class="uniList-item uniList-item-action">${data[index].name}</a>`);
+                $("#universityList").append(`<a target="_blank" href="${data[index].web_pages[0]}" class="list-group-item list-group-item-action">${data[index].name}</a>`);
 
             }
+        } else {
+            $("#universityList").append("Can't load the list from url.");
         }
     });
 });
