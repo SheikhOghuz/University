6600d72dd5456e45fa0953d8d8c701696953ab8a
Oghuz Ali <113483494+SheikhOghuz@users.noreply.github.com>
2022-12-04 21:14:00 -0800

    Dropdown
    FIND SOLUTION QUICK AHHHHH

--- university.js
+++ university.js
@@ -9,4 +9,14 @@ $(document).ready(function() {
             $("#universityList").append("Can't load the list from url.");
         }
     });
+    $.get("http://universities.hipolabs.com/search?country=United%20States", function(data, status) {
+        if (status == "success" && data != null && data != undefined) {
+            for (let index = 0; index < data.length; index++) {
+                $("#universityList").append(`<a target="_blank" href="${data[index].web_pages[0]}" class="list-group-item list-group-item-action">${data[index].name}</a>`);
+
+            }
+        } else {
+            $("#universityList").append("Can't load the list from url.");
+        }
+    });
 });
